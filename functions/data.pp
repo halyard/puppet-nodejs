42218f3ddd36784d2b0173c949f2a777f8e4b850
Les Aker <me@lesaker.org>
2015-08-14 20:58:27 -0700

    less looping

--- functions/data.pp
+++ functions/data.pp
@@ -4,10 +4,10 @@ function nodejs::data {
     'nodejs::provider' => 'nodenv',
     'nodejs::user' => "${::boxen_user}",
     'nodejs::build::ensure' => 'f18b3d67756d1cb25ba6e35044f816fd67211b33',
-    'nodejs::build::prefix' => "${nodejs::prefix}/node-build",
-    'nodejs::build::user' => "${nodejs::user}",
+    'nodejs::build::prefix' => hiera('nodejs::prefix') + '/node-build',
+    'nodejs::build::user' => "${::boxen_user}",
     'nodejs::nodenv::ensure' => 'v0.2.0',
-    'nodejs::nodenv::prefix' => "${nodejs::prefix}/nodenv",
-    'nodejs::nodenv::user' => "${nodejs::user}"
+    'nodejs::nodenv::prefix' => hiera('nodejs::prefix') + '/nodenv',
+    'nodejs::nodenv::user' => "${::boxen_user}"
   }
 }
