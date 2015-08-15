413b6026adc04e732381cdfd5a06095f04b34a1f
Les Aker <me@lesaker.org>
2015-08-14 21:01:16 -0700

    more looping

--- functions/data.pp
+++ functions/data.pp
@@ -1,13 +1,15 @@
 function nodejs::data {
-  {
-    'nodejs::prefix' => "${::boxen::config::home}",
+  $user = $::boxen_user
+  $prefix = $::boxen::config::home
+  $results = {
+    'nodejs::prefix' => "${prefix}",
     'nodejs::provider' => 'nodenv',
-    'nodejs::user' => "${::boxen_user}",
+    'nodejs::user' => "${user}",
     'nodejs::build::ensure' => 'f18b3d67756d1cb25ba6e35044f816fd67211b33',
-    'nodejs::build::prefix' => hiera('nodejs::prefix') + '/node-build',
-    'nodejs::build::user' => "${::boxen_user}",
+    'nodejs::build::prefix' => "${prefix}/node-build",
+    'nodejs::build::user' => "${user}",
     'nodejs::nodenv::ensure' => 'v0.2.0',
-    'nodejs::nodenv::prefix' => hiera('nodejs::prefix') + '/nodenv',
-    'nodejs::nodenv::user' => "${::boxen_user}"
+    'nodejs::nodenv::prefix' => "${prefix}/nodenv",
+    'nodejs::nodenv::user' => "${user}"
   }
 }
